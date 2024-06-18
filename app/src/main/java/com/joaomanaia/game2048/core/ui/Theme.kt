bb181755349adf0958951faf5f1b55a37e640df1
joaomanaia <joaopfmanaia@gmail.com>
2024-06-18 13:49:19 +0100

    Fix theme indentation

--- app/src/main/java/com/joaomanaia/game2048/core/ui/Theme.kt
+++ app/src/main/java/com/joaomanaia/game2048/core/ui/Theme.kt
@@ -14,60 +14,60 @@ import androidx.core.view.WindowCompat
 import com.google.android.material.color.MaterialColors
 
 private val LightThemeColors = lightColorScheme(
-	primary = md_theme_light_primary,
-	onPrimary = md_theme_light_onPrimary,
-	primaryContainer = md_theme_light_primaryContainer,
-	onPrimaryContainer = md_theme_light_onPrimaryContainer,
-	secondary = md_theme_light_secondary,
-	onSecondary = md_theme_light_onSecondary,
-	secondaryContainer = md_theme_light_secondaryContainer,
-	onSecondaryContainer = md_theme_light_onSecondaryContainer,
-	tertiary = md_theme_light_tertiary,
-	onTertiary = md_theme_light_onTertiary,
-	tertiaryContainer = md_theme_light_tertiaryContainer,
-	onTertiaryContainer = md_theme_light_onTertiaryContainer,
-	error = md_theme_light_error,
-	errorContainer = md_theme_light_errorContainer,
-	onError = md_theme_light_onError,
-	onErrorContainer = md_theme_light_onErrorContainer,
-	background = md_theme_light_background,
-	onBackground = md_theme_light_onBackground,
-	surface = md_theme_light_surface,
-	onSurface = md_theme_light_onSurface,
-	surfaceVariant = md_theme_light_surfaceVariant,
-	onSurfaceVariant = md_theme_light_onSurfaceVariant,
-	outline = md_theme_light_outline,
-	inverseOnSurface = md_theme_light_inverseOnSurface,
-	inverseSurface = md_theme_light_inverseSurface,
-	inversePrimary = md_theme_light_inversePrimary,
+    primary = md_theme_light_primary,
+    onPrimary = md_theme_light_onPrimary,
+    primaryContainer = md_theme_light_primaryContainer,
+    onPrimaryContainer = md_theme_light_onPrimaryContainer,
+    secondary = md_theme_light_secondary,
+    onSecondary = md_theme_light_onSecondary,
+    secondaryContainer = md_theme_light_secondaryContainer,
+    onSecondaryContainer = md_theme_light_onSecondaryContainer,
+    tertiary = md_theme_light_tertiary,
+    onTertiary = md_theme_light_onTertiary,
+    tertiaryContainer = md_theme_light_tertiaryContainer,
+    onTertiaryContainer = md_theme_light_onTertiaryContainer,
+    error = md_theme_light_error,
+    errorContainer = md_theme_light_errorContainer,
+    onError = md_theme_light_onError,
+    onErrorContainer = md_theme_light_onErrorContainer,
+    background = md_theme_light_background,
+    onBackground = md_theme_light_onBackground,
+    surface = md_theme_light_surface,
+    onSurface = md_theme_light_onSurface,
+    surfaceVariant = md_theme_light_surfaceVariant,
+    onSurfaceVariant = md_theme_light_onSurfaceVariant,
+    outline = md_theme_light_outline,
+    inverseOnSurface = md_theme_light_inverseOnSurface,
+    inverseSurface = md_theme_light_inverseSurface,
+    inversePrimary = md_theme_light_inversePrimary,
 )
 private val DarkThemeColors = darkColorScheme(
-	primary = md_theme_dark_primary,
-	onPrimary = md_theme_dark_onPrimary,
-	primaryContainer = md_theme_dark_primaryContainer,
-	onPrimaryContainer = md_theme_dark_onPrimaryContainer,
-	secondary = md_theme_dark_secondary,
-	onSecondary = md_theme_dark_onSecondary,
-	secondaryContainer = md_theme_dark_secondaryContainer,
-	onSecondaryContainer = md_theme_dark_onSecondaryContainer,
-	tertiary = md_theme_dark_tertiary,
-	onTertiary = md_theme_dark_onTertiary,
-	tertiaryContainer = md_theme_dark_tertiaryContainer,
-	onTertiaryContainer = md_theme_dark_onTertiaryContainer,
-	error = md_theme_dark_error,
-	errorContainer = md_theme_dark_errorContainer,
-	onError = md_theme_dark_onError,
-	onErrorContainer = md_theme_dark_onErrorContainer,
-	background = md_theme_dark_background,
-	onBackground = md_theme_dark_onBackground,
-	surface = md_theme_dark_surface,
-	onSurface = md_theme_dark_onSurface,
-	surfaceVariant = md_theme_dark_surfaceVariant,
-	onSurfaceVariant = md_theme_dark_onSurfaceVariant,
-	outline = md_theme_dark_outline,
-	inverseOnSurface = md_theme_dark_inverseOnSurface,
-	inverseSurface = md_theme_dark_inverseSurface,
-	inversePrimary = md_theme_dark_inversePrimary,
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    primaryContainer = md_theme_dark_primaryContainer,
+    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
+    secondary = md_theme_dark_secondary,
+    onSecondary = md_theme_dark_onSecondary,
+    secondaryContainer = md_theme_dark_secondaryContainer,
+    onSecondaryContainer = md_theme_dark_onSecondaryContainer,
+    tertiary = md_theme_dark_tertiary,
+    onTertiary = md_theme_dark_onTertiary,
+    tertiaryContainer = md_theme_dark_tertiaryContainer,
+    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
+    error = md_theme_dark_error,
+    errorContainer = md_theme_dark_errorContainer,
+    onError = md_theme_dark_onError,
+    onErrorContainer = md_theme_dark_onErrorContainer,
+    background = md_theme_dark_background,
+    onBackground = md_theme_dark_onBackground,
+    surface = md_theme_dark_surface,
+    onSurface = md_theme_dark_onSurface,
+    surfaceVariant = md_theme_dark_surfaceVariant,
+    onSurfaceVariant = md_theme_dark_onSurfaceVariant,
+    outline = md_theme_dark_outline,
+    inverseOnSurface = md_theme_dark_inverseOnSurface,
+    inverseSurface = md_theme_dark_inverseSurface,
+    inversePrimary = md_theme_dark_inversePrimary,
 )
 
 @Composable
@@ -81,21 +81,24 @@ fun Game2048Theme(
             val context = LocalContext.current
             if (useDarkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
+
         useDarkTheme -> DarkThemeColors
         else -> LightThemeColors
     }
 
-	val view = LocalView.current
-	if (!view.isInEditMode) {
-		val currentWindow = (view.context as? Activity)?.window
-			?: throw Exception("Not in an activity - unable to get Window reference")
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        val currentWindow = (view.context as? Activity)?.window
+            ?: throw Exception("Not in an activity - unable to get Window reference")
 
-		SideEffect {
-			currentWindow.statusBarColor = colorScheme.primary.toArgb()
+        SideEffect {
+            currentWindow.statusBarColor = colorScheme.primary.toArgb()
 
-			WindowCompat.getInsetsController(currentWindow, view).isAppearanceLightStatusBars = useDarkTheme
-		}
-	}
+            WindowCompat
+                .getInsetsController(currentWindow, view)
+                .isAppearanceLightStatusBars = useDarkTheme
+        }
+    }
 
     val colorsWithHarmonizedError = setupErrorColors(colorScheme, !useDarkTheme)
     val extendedColors = setupCustomColors(colorScheme, !useDarkTheme)
@@ -116,7 +119,8 @@ private fun setupErrorColors(
     colorScheme: ColorScheme,
     isLight: Boolean
 ): ColorScheme {
-    val harmonizedError = MaterialColors.harmonize(colorScheme.error.toArgb(), colorScheme.primary.toArgb())
+    val harmonizedError =
+        MaterialColors.harmonize(colorScheme.error.toArgb(), colorScheme.primary.toArgb())
     val roles = MaterialColors.getColorRoles(harmonizedError, isLight)
 
     //returns a colorScheme with newly harmonized error colors
